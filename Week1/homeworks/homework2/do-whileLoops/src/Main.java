efa79b38892561f126fadfd15914428152f90be4
ezgikahraman <kahraman.ezgi22@gmail.com>
2022-09-16 02:42:46 +0300

    Add do-whileLoops

---
+++ Week1/homeworks/homework2/do-whileLoops/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int j=100;
+        do {
+            System.out.println(j);
+            j+=2;
+        } while(j<10);
+        System.out.println("döngü bitti");
+    }
+}
