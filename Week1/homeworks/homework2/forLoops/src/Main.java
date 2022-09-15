bacb5efdd995a4cffcd9539523f1b2e55fe14a60
ezgikahraman <kahraman.ezgi22@gmail.com>
2022-09-16 02:17:51 +0300

    Add forLoops

---
+++ Week1/homeworks/homework2/forLoops/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i=i;i<=10;i++){
+            System.out.println(i);
+        }
+        System.out.println("döngü bitti");
+    }
+}
