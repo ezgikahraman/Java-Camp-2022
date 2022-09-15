7464a7c96457090f249caa5730d4508d8570ea32
ezgikahraman <kahraman.ezgi22@gmail.com>
2022-09-16 00:48:41 +0300

    Add variables

--- Week1/homeworks/homework2/variables/src/Main.java
+++ Week1/homeworks/homework2/variables/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        int sayi1=3;
+        double sayi2=5.60;
+        char krktr='e';
+        boolean dogruMu=true;
+        System.out.println(sayi1);
+        System.out.println(sayi2);
+        System.out.println(krktr);
+        System.out.println(dogruMu);
     }
 }
