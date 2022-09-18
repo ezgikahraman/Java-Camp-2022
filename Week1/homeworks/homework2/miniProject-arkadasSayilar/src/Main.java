d51e7504e757544bebbb53bee218252e4c575042
ezgikahraman <kahraman.ezgi22@gmail.com>
2022-09-18 21:57:19 +0300

    Add miniProject-arkadasSayilar

---
+++ Week1/homeworks/homework2/miniProject-arkadasSayilar/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+    //İki sayı birbirinin kendisi hariç bölenleri toplamına eşitse bu sayılara arkadaş sayılar denir.
+        int sayi1=220;
+        int sayi2=284;
+        int toplam1=0;
+        int toplam2=0;
+
+        for(int i=1;i<sayi1;i++) {
+            if(sayi1%i==0) {
+                toplam1+=i;
+            }
+        }
+        for(int i=1;i<sayi2;i++) {
+            if(sayi2%i==0) {
+                toplam2+=i;
+            }
+        }
+
+        if(sayi1==toplam2 && sayi2==toplam1) {
+            System.out.println("bu iki sayı arkadaştır.");
+        }
+        else {
+            System.out.println("bu iki sayı arkadaş değildir.");
+        }
+    }
+}
