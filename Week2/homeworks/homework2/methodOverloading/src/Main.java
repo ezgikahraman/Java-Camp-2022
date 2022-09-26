ce05a7cf79a2b0a9fd9711607100b19a993210a9
Ezgi Kahraman <46925745+ezgikahraman@users.noreply.github.com>
2022-09-27 00:56:12 +0300

    Add methodOverloading

---
+++ Week2/homeworks/homework2/methodOverloading/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Transactions transactions = new Transactions();
+        System.out.println("Total : "+transactions.addition(5,6));
+        System.out.println("Total : "+transactions.addition(5,6,7));
+    }
+}
